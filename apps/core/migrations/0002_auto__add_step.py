e51d22e62abbc2c4127aca567982f9c6d576a250
Marcos Daniel Petry <marcospetry@gmail.com>
2014-01-03 01:02:09 -0200

    added step migrations

---
+++ apps/core/migrations/0002_auto__add_step.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Step'
+        db.create_table(u'core_step', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('board', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Board'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('next', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Step'])),
+        ))
+        db.send_create_signal(u'core', ['Step'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Step'
+        db.delete_table(u'core_step')
+
+
+    models = {
+        u'core.board': {
+            'Meta': {'object_name': 'Board'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'core.step': {
+            'Meta': {'object_name': 'Step'},
+            'board': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Board']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'next': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Step']"})
+        },
+        u'core.story': {
+            'Meta': {'object_name': 'Story'},
+            'board': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Board']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['core']
