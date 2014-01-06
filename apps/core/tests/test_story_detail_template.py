42b20e4305738d19d702e557aade3ec048469bbe
Marcos Daniel Petry <marcospetry@gmail.com>
2014-01-06 00:51:41 -0200

    fix story test

--- apps/core/tests/test_story_detail_template.py
+++ apps/core/tests/test_story_detail_template.py
@@ -4,7 +4,6 @@ from lxml import html
 from apps.core.models import Story, Step
 from apps.core.views import StoryDetailView
 
-
 class StoryDetailViewTest(TestCase):
     urls = 'apps.core.urls'
 
@@ -17,6 +16,6 @@ class StoryDetailViewTest(TestCase):
         self.dom = html.fromstring(response.rendered_content)
 
     def test_should_have_story_name_on_title(self):
-        title = self.dom.cssselect('h1')[0]
-        self.assertEqual(title.text, self.story.name)
+        title = self.dom.cssselect('.modal-dialog .modal-content h4.modal-title')[0]
+        self.assertEqual(title.text, "Story #{0} - {1}".format(self.story.id, self.story.name) )
 
