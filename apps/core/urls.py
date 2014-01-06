6958604f6e7be68fc827435720a4efd134af856e
Marcos Daniel Petry <marcospetry@gmail.com>
2014-01-06 00:15:18 -0200

    added jquery

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 from apps.core.views import BoardDetailView, StoryDetailView
 
 urlpatterns = patterns('',
-    #...
     url(r'^boards/(?P<pk>\d+)/$', BoardDetailView.as_view(), name='board-detail'),
     url(r'^story/(?P<pk>\d+)/$', StoryDetailView.as_view(), name='story-detail'),
 )
