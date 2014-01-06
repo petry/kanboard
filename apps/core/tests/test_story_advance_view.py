435d45184872858130a9856d26651e77d86a97de
Marcos Daniel Petry <marcospetry@gmail.com>
2014-01-06 15:10:19 -0200

    added story advance redirect view

---
+++ apps/core/tests/test_story_advance_view.py
@@ -0,0 +1,31 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase, RequestFactory
+from model_mommy import mommy
+from apps.core.models import Board, Step, Story, BoardPosition
+from apps.core.views import StoryAdvanceView
+
+
+class BoardListViewTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.board = mommy.make(Board)
+        self.step2 = Step.objects.create(board=self.board, name='step 2')
+        self.step1 = Step.objects.create(board=self.board, name='step 1',
+                                         next=self.step2, initial=True)
+        self.story = mommy.make(Story)
+        self.position = BoardPosition.objects.create(
+            story=self.story,
+            board=self.board,
+            status=self.step1
+        )
+
+        self.request = self.factory.get('/boards/1/advance/')
+        self.response = StoryAdvanceView.as_view()(self.request, pk=self.story.pk)
+
+    def test_should_redirect_to_board(self):
+        self.assertEqual(self.response.url, reverse('board-detail', kwargs={'pk': self.board.id}))
+        self.assertEqual(dict(self.response.items())['Location'], reverse('board-detail', kwargs={'pk': self.board.id}))
+
+        
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+from django.views.generic.base import RedirectView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from apps.core.models import Board, Story
@@ -11,3 +13,9 @@ class BoardDetailView(DetailView):
 
 class StoryDetailView(DetailView):
     model = Story
+
+class StoryAdvanceView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        story = Story.objects.get(id=1)
+        return reverse('board-detail', kwargs={'pk': story.boardposition.board.id})
