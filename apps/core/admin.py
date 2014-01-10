a31d27a2523251870f05c2eb0d92079dc9542b48
Marcos Daniel Petry <marcospetry@gmail.com>
2014-01-10 12:45:23 -0200

    customize transition admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -15,10 +15,14 @@ class IssueAdmin(admin.ModelAdmin):
     list_display = ['name']
     inlines = [PositionInline]
 
+
 class BoardAdmin(admin.ModelAdmin):
     inlines = [StepInline]
 
 
+class TransitionAdmin(admin.ModelAdmin):
+    list_display = ('issue', 'step', 'date')
+
 admin.site.register(Board, BoardAdmin)
 admin.site.register(Issue, IssueAdmin)
-admin.site.register(Transition)
+admin.site.register(Transition, TransitionAdmin)
--- apps/core/tests/test_admin.py
+++ apps/core/tests/test_admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.contrib import admin as django_admin
-from apps.core.admin import IssueAdmin, BoardAdmin
+from apps.core.admin import IssueAdmin, BoardAdmin, TransitionAdmin
 from apps.core.models import Board, Issue, Transition
 
 
@@ -23,3 +23,6 @@ class CoreAdminTest(TestCase):
 
     def test_Transition_model_should_be_registered_within_the_admin(self):
         self.assertIn(Transition, django_admin.site._registry)
+
+    def test_transition_should_be_customize(self):
+        self.assertTrue(django_admin.site._registry[Transition], TransitionAdmin)
